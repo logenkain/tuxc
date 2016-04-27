188e5a2428322c55d50fe4587575ad39a318718b
Logen Kain <logen@sudotask.com>
2016-04-26 23:23:48 -0700

    tuxc: Added execution

--- tuxc.c
+++ tuxc.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 int check_file(char *pkg_mgr);
+void help(void);
+
+int DEBUG=1;
 
 //Main
 
 int main(int argc, char *argv[])
 {
-	int i;
-	i = 0;
+	int i = 0;
+	char *PKM;
+	char switches[10];
+	char package[50];
+	char command[100];
+  
 
+	//Add package managers to check for
   char *pkg_mgr[] = {
 	  "xbps-install", "apt-get",
 	  "pacman", "yum"
@@ -19,10 +28,30 @@ int main(int argc, char *argv[])
 	for (i=0; i < sizeof(pkg_mgr) / sizeof(pkg_mgr[0]); i++) {
     
 		if (check_file(pkg_mgr[i]) == 0){
+			PKM = pkg_mgr[i];
 			break;
 		}
 	}
-	printf ("%s\n",pkg_mgr[i]);
+	if (DEBUG == 1){
+		printf ("%s\n",PKM);
+	}
+
+
+	// placeholders
+	strcpy(switches, "doctor"); 
+  strcpy(package, "test package");
+	// end placeholders
+
+  strcat(command, PKM);
+	strcat(command, " ");
+	strcat(command, switches);
+	strcat(command, " ");
+	strcat(command, package);
+	
+	printf("%s\n", command);
+	
+//	system(command);
+	
 	return 0;
 }
 
@@ -38,10 +67,12 @@ int check_file(char *pkg_mgr)
 	
 	
 	if (access( path, F_OK ) != -1 ) {
-		printf( "%s exists\n", path);
 		return 0;
 	}else{
-		printf( "%s does NOT exist\n", path);
 		return 1;
 	}
+}
+
+void help(void){
+	printf("This is the future help file!!!\n");
 }
