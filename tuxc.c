a8026969e6ddff6dd1e8bbd7f2630fc8b28c1bed
Logen Kain <logen@sudotask.com>
2016-05-09 22:54:40 -0700

    Added working example with XBPS

--- tuxc.c
+++ tuxc.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	int i;
 	char *pkgMgr;
 	char switches[SWITCHLENGTH];
-	char package[PACKAGELENGTH];
+	char package[PACKAGELENGTH] = "FALSE";
 	char command[MAXLINE];
 
 	//Add package managers to check for
@@ -51,9 +51,8 @@ int main(int argc, char *argv[])
   if (argv[2] && strlen(argv[2]) < PACKAGELENGTH){
 		strcpy(package, argv[2]);
 	}
-	else{
-		help();
-		return 1;
+	else if (strcmp(argv[1], "s")){
+		//do nothing
 	}
 
 	// Load package manager specific shit here
@@ -61,40 +60,105 @@ int main(int argc, char *argv[])
 	// make command = the base command... So if someone does 'tuxc s foobar'
 	// in the case of XBPS, command will equal "xbps-query -Rs" We add the space later
   
-	int example;
-  
 	char *searchCommand;
 	char *syncCommand;
 	char *installCommand;
+	char *reinstallCommand;
 	char *removeCommand;
 	char *cleanCommand;
+	char *purgeCommand;
+	char *upgradeCommand;
+
+/* The following will be praised from a file in the future
+ * this is only temporary
+ */
+
+	searchCommand = "xbps-query -Rs";
+	syncCommand = "xbps-install -S";
+	installCommand = "xbps-install";
+	reinstallCommand = "xbps-install -f";
+	removeCommand = "xbps-remove";
+	purgeCommand = "xbps-remove -R";
+	upgradeCommand = "xbps-install -Su";
+	cleanCommand="xbps-remove -O";
+
+	/*End temporary code
+	 */
 
 	if (strcmp(switches, "s") == 0 || 
-			strcmp(switches, "S") == 0 ||
 			strcmp(switches, "-s") == 0 ||
-			strcmp(switches, "-S") == 0){
+			strcmp(switches, "-search") == 0 ||
+			strcmp(switches, "--search") == 0){
 		strcpy(command, searchCommand);
 	}
 
+	else if (strcmp(switches, "sy") == 0 ||
+			strcmp(switches, "-sy") == 0 ||
+			strcmp(switches, "sync") == 0 ||
+			strcmp(switches, "--sync") == 0){
+		strcpy(command, syncCommand);
+	}
 	else if (strcmp(switches, "i") == 0 ||
-			strcmp(switches, "I") == 0 ||
 			strcmp(switches, "-i") == 0 ||
-			strcmp(switches, "-I") == 0){
+			strcmp(switches, "install") == 0 ||
+			strcmp(switches, "--install") == 0){
 		strcpy(command, installCommand);
+	}
+	else if (strcmp(switches, "ri") == 0 ||
+			strcmp(switches, "-ri") == 0 ||
+			strcmp(switches, "reinstall") == 0 ||
+			strcmp(switches, "--reinstall") == 0){
+		strcpy(command, reinstallCommand);
+	}
+	else if (strcmp(switches, "r") == 0 ||
+			strcmp(switches, "-r") == 0 ||
+			strcmp(switches, "remove") == 0 ||
+			strcmp(switches, "--remove") == 0){
+		strcpy(command, removeCommand);
+	}
+	else if (strcmp(switches, "p") == 0 ||
+			strcmp(switches, "-p") == 0 ||
+			strcmp(switches, "purge") == 0 ||
+			strcmp(switches, "--purge") == 0){
+		strcpy(command, purgeCommand);
+	}
+	else if (strcmp(switches, "u") == 0 ||
+			strcmp(switches, "-u") == 0 ||
+			strcmp(switches, "upgrade") == 0 ||
+			strcmp(switches, "--upgrade") == 0){
+		strcpy(command, upgradeCommand);
+	}
+	else if (strcmp(switches, "c") == 0 ||
+			strcmp(switches, "-c") == 0 ||
+			strcmp(switches, "clean") == 0 ||
+			strcmp(switches, "--clean") == 0){
+		strcpy(command, cleanCommand);
+	}
+
+
+	
+	else{
+		help();
+		return 1;
 	}
 
 	
 	////////////////////////////////////////
 
 	
-	strcat(command, " ");
-	strcat(command, package);
+	/*Prevent catting an empty variable
+	 */
+
+	if (strcmp(package,"FALSE")){
+		strcat(command, " ");
+	  strcat(command, package);
+	}
   
 	if (DEBUG==TRUE){
 		printf("%s\n", command);
 	}
 	
-//	system(command);
+	system(command);
 	return 0;
 }
 
