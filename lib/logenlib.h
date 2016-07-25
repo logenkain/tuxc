4ad99156223c5b33eec1ad8b85470fcdd070102e
Logen Kain <logen@sudotask.com>
2016-07-25 10:51:39 -0700

    Updated library

--- lib/logenlib.h
+++ lib/logenlib.h
@@ -1,9 +1,15 @@
 #ifndef LOGENLIB_H_INCLUDED
 #define LOGENLIB_H_INCLUDED
 #include<lua.h>
-#include<stdio.h>
 
+/**
+ * Check common bin files (does not use PATH variable
+ */
 int check_bin(char*, int debug);
+/**
+ * Load a config file with lua (currently only supports strings)
+ */
 void load_config(char *filename, int number_of_args, ...);
+
 void l_error (lua_State *L, const char *fmt, ...);
 #endif
